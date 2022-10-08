ae06a62f03dbef741e543ecd320567ad9aaa439f
danghai1412 <haipdgch200436@fpt.edu.vn>
2022-10-08 13:00:34 +0700

    ok

--- routes/mobile.js
+++ routes/mobile.js
@@ -10,6 +10,14 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/api", (req, res) => {
+  MobileModel.find((err, data) => {
+    if (!err) {
+      res.json(data);
+    }
+  });
+});
+
 router.get("/add", (req, res) => {
   res.render("mobile/add");
 });
@@ -35,5 +43,31 @@ router.get("/detail/:id", (req, res) => {
     }
   });
 });
+
+router.get("/delete/:id", (req, res) => {
+  MobileModel.findByIdAndDelete(req.params.id, (err) => {
+    if (!err) {
+      console.log("Delete mobile succeed !")
+      res.redirect("/mobile")
+    }
+  })
+})
+
+router.get("/edit/:id", (req, res) => {
+   MobileModel.findById(req.params.id, (err, data) => {
+     if (!err) {
+        res.render("mobile/edit", { mobile: data })
+     }
+   })
+})
+
+router.post("/edit/:id", (req, res) => {
+    MobileModel.findByIdAndUpdate(req.params.id, req.body, (err) => {
+      if (!err) {
+        console.log("Edit mobile succeed !")
+        res.redirect("/mobile")
+      }
+    })
+})
 
 module.exports = router;
